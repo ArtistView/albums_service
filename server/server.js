a9e79675ebe1a780a1aa95171f945893a3341895
mjrosenberg <mjr70@duke.edu>
2020-06-17 10:44:33 -0700

    set up basic structure for the albums component

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const Promise = require('bluebird');
+const PORT = 3273;
+const app = express();
+
+//add in express static for the public folder
+app.use(bodyParser());
+app.use(cors());
+//need to instantiate the database and then promisify
+
+app.listen(PORT, ()=>{
+  console.log('listening on port', PORT);
+})
