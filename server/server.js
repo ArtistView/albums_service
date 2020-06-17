17c0bcd0a0c5c4ced1f1eb9469a787555f843778
mjrosenberg <mjr70@duke.edu>
2020-06-17 11:12:44 -0700

    server loads the index.html file

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const Promise = require('bluebird');
 const PORT = 3273;
 const app = express();
 
+app.use(express.static('public'));
 //add in express static for the public folder
 app.use(bodyParser());
 app.use(cors());
