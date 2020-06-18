e70d4d8731be16c6bbd4ba0fd01ff9f952ea428f
mjrosenberg <mjr70@duke.edu>
2020-06-18 11:52:39 -0700

    added a quick sample test for app.jsx

---
+++ tests/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+describe('App', () => {
+  it('shole be true', () => {
+    const foo = true;
+    expect(foo).toBe(true);
+  });
+});
