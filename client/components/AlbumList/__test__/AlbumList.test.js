c2a10b363670380cb57608d4059f4bdc41dcbb59
mjrosenberg <mjr70@duke.edu>
2020-06-18 09:53:13 -0700

    created test files for my components

---
+++ client/components/AlbumList/__test__/AlbumList.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import AlbumList from '../AlbumList';
+// import { isTSAnyKeyword } from '@babel/types';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDom.render(<AlbumList />, div);
+  // not working with jsx for some reason, also need to add in a test script
+});
