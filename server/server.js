06c91bd25d7c54d89d6018826cb23fc4fbbea6a6
mjrosenberg <mjr70@duke.edu>
2020-06-18 17:31:32 -0700

    database set up

--- server/server.js
+++ server/server.js
@@ -10,7 +10,45 @@ app.use(express.static('public'));
 app.use(bodyParser());
 app.use(cors());
 // need to instantiate the database and then promisify
+const db = require('../database/db.js');
+const artists = require('../../top_songs_service/Songs.js');
 
+Promise.promisifyAll(require('mongoose'));
+// console.log(artists[0], artists[1]);
+// for (var artist of artists) {
+//   var artist1 = new db.Song(artist);
+//   artist1.save(() => {
+//   console.log('cool');
+//   });
+// }
+// app.get('/songs', (req, res) => {
+//   db.Song.find()
+//     .then((data) => {
+//       for (var item of data) {
+//         console.log(item.albumId);
+//         db.Album.updateOne({ _id: item.albumId },{$push: {songs: item._id}},{},(album)=>{
+//           console.log('album found', album)
+//         });
+//         // .then((album)=> {
+//         //   console.log(album[0], item._id, album[0].songs);
+//         //   album[0].songs.push(item._id);
+//         // })
+//         // .catch((err)=>{
+//         //   res.send(err);
+//         // }); // { $push: {songs: item._id} });
+//       }
+//       res.send('done');
+//     });
+// });
+// const artist1 = new db.Album({
+//   name: 'Squarepants',
+//   artistId: '5eebdbf1bf2d490c13ff868d',
+//   type: 'album',
+//   imageUrl: 'https://tinyurl.com/ya65klnb',
+// });
+// artist1.save(() => {
+//   console.log('cool');
+// });
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 });
