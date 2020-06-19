f054c5538874e7fd60a6116d9f4ab7ced62c2976
mjrosenberg <mjr70@duke.edu>
2020-06-19 16:40:18 -0700

    trying to figure out play button

--- client/components/Album/Album.jsx
+++ client/components/Album/Album.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import {GrCirclePlay} from 'react-icons/gr';
 
 const Album = (props) => {
   // props should be an album object with properties for the title, cover image, first song and key and onclick and onhover handlers that play the song
-  console.log(props);
   // adding this comment to test circle
   return (
     <div className="album" id={props.key}>
       {/* <script src="./Album.css" /> */}
+      {/* <h3 id="gr-play"> */}
       <img id="album-cover" src={props.album.imageUrl} alt="" />
+        {/* <GrCirclePlay />
+      </h3> */}
       {/* need a hovering play button that goes over the image and appears on hover onClick for the play button should call ()=>{props.onClick(props.album.firstSong)} */}
       <div id="album-title">
         {props.album.title}
