dccb56dd9fd7b30d88038623816658358ec6b434
mjrosenberg <mjr70@duke.edu>
2020-06-27 22:39:31 -0700

    implementing play/pause tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,3 +1,5 @@
+const playPauseList = ['https://fakespotify.s3-us-west-1.amazonaws.com/play-button-2.png', 'https://fakespotify.s3-us-west-1.amazonaws.com/pause-button-try-3.png'];
+
 describe('My App Tests', () => {
   it('loads the page', () => {
     cy.visit('http://localhost:3273');
@@ -6,11 +8,19 @@ describe('My App Tests', () => {
     cy.contains(/Albums|Singles and EPs|Collaborations|Appears On/g);
     // check if the app contains any of those texts
   });
-  it('album cover gets darker on hover', () => {
-    cy.get('#album-cover').first().should('have.css', 'opacity', '1'); // the album cover should start with an opacity of one when it's not hovered over
-    cy.get('.album').first().invoke('mouseover') // hovering over the first album
-      .then(cy.get('#album-cover').should('have.css', 'opacity', '.5')); // check if the opacity changed
-    // cy.get('.album').first().trigger('mouseleave'); // stop hovering over the first album
-    cy.get('#album-cover').first().should('have.css', 'opacity', '1'); // check if the opacity changed back
+  // it('album cover gets darker on hover', () => {
+  //   cy.get('#album-cover').first().should('have.css', 'opacity', '1'); // the album cover should start with an opacity of one when it's not hovered over
+  //   cy.get('.album').first().invoke('mouseover') // hovering over the first album
+  //     .then(cy.get('#album-cover').should('have.css', 'opacity', '.5')); // check if the opacity changed
+  //   // dont think i can even test this because hover using trigger doesn't change the css
+  //   // cy.get('.album').first().trigger('mouseleave'); // stop hovering over the first album
+  //   cy.get('#album-cover').first().should('have.css', 'opacity', '1'); // check if the opacity changed back
+  // });
+  it('play/pause toggles on click', () => { // check if the play button switches to pause on click
+    cy.get('#play-button').first().should('have.attr', 'src').should('include', playPauseList[0]); // checks if it shows play at first
+    cy.get('#play-button').first().click({ force: true }); // clicks the button (force must be true bc it only turns visible on hover and this doesn't mess with css)
+    cy.get('#play-button').first().should('have.attr', 'src').should('include', playPauseList[1]); // checks if it shows pause
+    cy.get('#play-button').first().click({ force: true }); // changes it back
+    cy.get('#play-button').first().should('have.attr', 'src').should('include', playPauseList[0]); // checks if it shows play again
   });
 });
