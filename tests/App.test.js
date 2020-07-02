7d6f6cfac5530e8932ecf9e3ce6259175c7cc8a1
mjrosenberg <mjr70@duke.edu>
2020-07-01 17:20:57 -0700

    app tests written but not running

--- tests/App.test.js
+++ tests/App.test.js
@@ -10,14 +10,21 @@ import App from '../client/App.jsx';
 import AlbumList from '../../client/components/AlbumList/AlbumList.jsx';
 
 describe('App', () => {
+  const wrapper = shallow(<App />);
   it('renders correctly in \'debug mode\' ', () => {
-    // TODO
     const component = shallow(<App debug />);
     expect(component).toMatchSnapshot();
   });
-  it('has at least one album list', () => {
-    // TODO
-    expect(wrapper.contains(AlbumList)).toBe(true);
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
   });
+  it('has state', () => {
+    expect(wrapper.state('artist')).toBe('5eebdbf1bf2d490c13ff868d');
+    expect(wrapper.state('playing')).toBe('');
+  });
+  // it('has at least one album list', () => {
+  //   // TODO
+  //   expect(wrapper.contains(AlbumList)).toBe(true);
+  // });
   // maybe add more tests about the state
 });
